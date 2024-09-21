50f2d5c2f9e0be4c39301425313640e4618e6402
chance9077 <675409431@qq.com>
2024-09-21 12:11:26 +0800

    update

--- conf.d/00_env.fish
+++ conf.d/00_env.fish
@@ -1,3 +1,4 @@
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
-export HOMEBREW_BOTTLE_DOMAIN="https://mirrors.aliyun.com/homebrew/homebrew-bottles"
+export HOMEBREW_API_DOMAIN="https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles/api"
+export HOMEBREW_BOTTLE_DOMAIN="https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles"
